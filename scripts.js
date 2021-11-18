aae5d32efb747aace170f2af390ccec2f1fa74a6
Sean Orosz <seanorosz@yahoo.com>
2021-11-18 16:26:41 -0500

    Added another Algorithm

--- scripts.js
+++ scripts.js
@@ -22,6 +22,78 @@ const reverseString = (arg) => {
     return ans;
 }
 
+const factorialize = (arg) => {
+    ans = 1;
+    let num = parseInt(arg);
+    if(num !== 0) {
+        for (let x = 1; x < num + 1; x++){
+            console.log(ans + ' ' + x);
+            ans *= x;
+        }
+    }
+    return ans;
+}
+
+const longestWord = (arg) => {
+    ans = 0;
+    let words = arg.split(' ');
+    words.forEach(word => {
+        console.log(word + ' ' + word.length);
+        if(word.length > ans){
+            ans = word.length;
+        }
+    });
+    return ans;
+}
+
+const longestNum = (arg) => {
+    let nums = arg.split(',');
+    let array = [];
+    nums.forEach(num => {
+        if(num.indexOf('[') !== -1){
+            num = num.split('[');
+            num.forEach(x => {
+                if(x !== '' && x.indexOf(']') === -1){
+                    array.push(x);
+                }
+            });
+        } else {
+            array.push(parseInt(num));
+        }
+    });
+    let arrays = array.toString().split(' ');
+    let finalArrays = arrays.toString().split(',');
+    let subArray = [];
+    let finalArray = [];
+        for(let i = 0; i < finalArrays.length; i ++){
+            if(finalArrays[i] !== ''){
+                subArray.push(finalArrays[i]);
+            } if(finalArrays[i] === '' || i === finalArrays.length - 1){
+                if(subArray.length !== 0){
+                    finalArray.push(subArray);
+                }
+                subArray = [];
+            }
+        }
+        let ansArray = [];
+        let highNum = '';
+    for(let x = 0; x < finalArray.length; x++){
+        for(let y = 0; y < finalArray[x].length; y++){
+            if(y === 0){
+                highNum = finalArray[x][y];
+            }
+            console.log(`high: ${highNum}, arr: ${finalArray[x][y]}`);
+            if(parseInt(finalArray[x][y]) > parseInt(highNum)){
+                highNum = finalArray[x][y];
+            }
+        }
+        ansArray.push(highNum);
+        highNum = '';
+        console.log('');
+    }
+    return ansArray;
+}
+
 const setDisplay = (ev) => {
     if(el !== ev.target){
         el = ev.target;
@@ -48,7 +120,10 @@ setDefaultDisplay();
 
 let Functions = {
     tempConvert,
-    reverseString
+    reverseString,
+    factorialize,
+    longestWord,
+    longestNum
 }
 
 button.addEventListener('click', submitInput);
