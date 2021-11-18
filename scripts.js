a245deb824d7818ebbef3d9b2c21a2af73cfd510
Sean Orosz <seanorosz@yahoo.com>
2021-11-18 12:27:30 -0500

    Added functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
 const submitInput = (event) => {
     event.preventDefault();
     output.style.display = 'block';
-    output.innerHTML = solve(input.value);
+    output.innerHTML = Functions[currentFunc](input.value);
     input.value = '';
 }
 
-const solve = (arg) => {
+const tempConvert = (arg) => {
     let answer = `${((arg * (9/5)) + 32)}&deg`;
     return answer;
 };
 
+const anotherFunc = () => {
+    console.log('this is another function');
+}
+
 let button = document.getElementById('submitButton');
 let input = document.getElementById('inputBox');
 let output = document.getElementById('output');
+let currentProblem = document.getElementsByClassName('prob')[0];
+let currentFunc = currentProblem.dataset.func;
+
+let Functions = {
+    tempConvert
+}
 
 button.addEventListener('click', submitInput);
