81abc002bd9f32315b604f1a72f70011481eb627
Sean Orosz <seanorosz@yahoo.com>
2021-11-18 02:37:01 -0500

    v1.0

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 const submitInput = (event) => {
     event.preventDefault();
+    output.style.display = 'block';
     output.innerHTML = solve(input.value);
+    input.value = '';
 }
 
-const solve = (para) => (para * 2);
+const solve = (arg) => {
+    let answer = `${((arg * (9/5)) + 32)}&deg`;
+    return answer;
+};
 
 let button = document.getElementById('submitButton');
 let input = document.getElementById('inputBox');
