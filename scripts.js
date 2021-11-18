d732f6c719fbd23b17168e4c7edf48dd425faf96
Sean Orosz <seanorosz@yahoo.com>
2021-11-18 02:09:09 -0500

    Updated CSS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-const func1 = () => {}
+const submitInput = (event) => {
+    event.preventDefault();
+    output.innerHTML = solve(input.value);
+}
+
+const solve = (para) => (para * 2);
+
+let button = document.getElementById('submitButton');
+let input = document.getElementById('inputBox');
+let output = document.getElementById('output');
+
+button.addEventListener('click', submitInput);
